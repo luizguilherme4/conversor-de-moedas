8e8f342987f3b8176a172be99c1f38d461829d18
Luiz Guilherme <101076506+luizguilherme4@users.noreply.github.com>
2022-12-15 16:21:08 -0300

    aplicando conversão para todas as moedas

--- script.js
+++ script.js
@@ -1,25 +1,68 @@
 var valueRight = document.querySelector("#value-right");
 var coinLeft = document.querySelector("#coinLeft");
 var coinRight = document.querySelector("#coinRight");
-var symbol = document.querySelector("#symbol")
+var symbolLeft = document.querySelector("#symbol-left");
+var symbolRight = document.querySelector("#symbol-right");
 
 function convert() {
     var valueInput = document.querySelector("#value-left").value;
 
-    switch (coinLeft.value) {
-        case "dolar":
-            valueRight.value = valueInput * 5;
-            symbol.innerHTML = "USD";
-            break;
+    if (coinLeft.value == "real") {
+        symbolLeft.innerHTML = "R$";
 
-        case "euro":
-            valueRight.value = valueInput * 10;
-            symbol.innerHTML = "EUR";
-            break;
+        switch (coinRight.value) {
+            case "dolar":
+                valueRight.value = (valueInput / 5.31).toFixed(2);
+                symbolRight.innerHTML = "USD";
+                break;
 
-        case "real":
-            valueRight.value = valueInput;
-            symbol.innerHTML = "R$";
-            break;
+            case "euro":
+                valueRight.value = (valueInput / 5.64).toFixed(2);
+                symbolRight.innerHTML = "EUR";
+                break;
+
+            case "real":
+                valueRight.value = valueInput;
+                symbolRight.innerHTML = "R$";
+                break;
+        }
+    } else if (coinLeft.value == "dolar") {
+        symbolLeft.innerHTML = "USD";
+
+        switch (coinRight.value) {
+            case "dolar":
+                valueRight.value = valueInput;
+                symbolRight.innerHTML = "USD";
+                break;
+
+            case "euro":
+                valueRight.value = (valueInput / 1.06).toFixed(2);
+                symbolRight.innerHTML = "EUR";
+                break;
+
+            case "real":
+                valueRight.value = (valueInput * 5.31).toFixed(2);
+                symbolRight.innerHTML = "R$";
+                break;
+        }
+    } else {
+        symbolLeft.innerHTML = "EUR";
+
+        switch (coinRight.value) {
+            case "dolar":
+                valueRight.value = (valueInput * 1.06).toFixed(2);
+                symbolRight.innerHTML = "USD";
+                break;
+
+            case "euro":
+                valueRight.value = valueInput;
+                symbolRight.innerHTML = "EUR";
+                break;
+
+            case "real":
+                valueRight.value = (valueInput * 5.64).toFixed(2);
+                symbolRight.innerHTML = "R$";
+                break;
+        }
     }
 }
